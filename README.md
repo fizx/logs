e968eea34801c10e34207c1aea5eb19d385e84f2
Kyle Maxwell <kyle@kylemaxwell.com>
2013-04-09 19:45:39 -0700

    very simple logging

---
+++ README.md
@@ -0,0 +1,8 @@
+A (so far) very simple logging package for golang.  Go's default logger doesn't have log levels.  This adds log levels:
+
+- DEBUG
+- INFO
+- WARN
+- ERROR
+
+Also, I only care about the root logger.  Deal with it (or pull request).
---
+++ logs.go
@@ -0,0 +1,57 @@
+package logs
+
+import (
+	"log"
+)
+
+type Level int
+
+const (
+	DEBUG = iota
+	INFO
+	WARN
+	ERROR
+)
+
+var CurrentLevel Level
+
+func SetLevel(level Level) {
+	CurrentLevel = level
+}
+
+func Log(level Level, v ...interface{}) {
+	if level >= CurrentLevel {
+	  str := ""
+		switch level {
+		case DEBUG:
+			str = "DEBUG"
+		case INFO:
+			str = "INFO"
+		case WARN:
+			str = "WARN"
+		case ERROR:
+			str = "ERROR"
+		default:
+			panic("what level?")
+		}
+		first := v[0].(string)
+		remaining := v[1:]
+		log.Printf("["+str+"]"+first, remaining...)
+	}
+}
+
+func Debug(v ...interface{}) {
+	Log(DEBUG, v)
+}
+
+func Info(v ...interface{}) {
+	Log(INFO, v)
+}
+
+func Warn(v ...interface{}) {
+	Log(WARN, v)
+}
+
+func Error(v ...interface{}) {
+	Log(ERROR, v)
+}
