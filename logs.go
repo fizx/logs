0a7e127bcf248d19e8f15cc3ea884dc9c913dd59
Kyle Maxwell <kyle@kylemaxwell.com>
2013-04-09 20:27:47 -0700

    fix params passing, add space to output

--- logs.go
+++ logs.go
@@ -37,7 +37,7 @@ func Log(level Level, v ...interface{}) {
 		first, isString := v[0].(string)
 		remaining := v[1:]
 		if isString {
-			log.Printf("["+str+"]"+first, remaining...)
+			log.Printf("["+str+"] "+first, remaining...)
 		} else {
 			slice := []interface{}{"[" + str + "]", first}
 			slice = append(slice, remaining...)
@@ -47,17 +47,17 @@ func Log(level Level, v ...interface{}) {
 }
 
 func Debug(v ...interface{}) {
-	Log(DEBUG, v)
+	Log(DEBUG, v...)
 }
 
 func Info(v ...interface{}) {
-	Log(INFO, v)
+	Log(INFO, v...)
 }
 
 func Warn(v ...interface{}) {
-	Log(WARN, v)
+	Log(WARN, v...)
 }
 
 func Error(v ...interface{}) {
-	Log(ERROR, v)
+	Log(ERROR, v...)
 }
